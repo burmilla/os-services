8ac065e51fa73bdc7714cc9be56983d4be252787
Olli Janatuinen <olli.janatuinen@gmail.com>
2023-12-15 15:22:58 +0000

    Docker compose, support cli plugin format

--- images/20-dockercompose/download.sh
+++ images/20-dockercompose/download.sh
@@ -3,5 +3,7 @@
 VERSION=$(jq -r 'map(select(.prerelease == false)) | first | .tag_name' <<< $(wget -q -O - https://api.github.com/repos/docker/compose/releases))
 DOCKERARCH=$(uname -m)
 COMPOSE_URL="https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-linux-${DOCKERARCH}"
-wget -q -O /var/lib/rancher/compose/docker-compose $COMPOSE_URL
-chmod 0755 /var/lib/rancher/compose/docker-compose
+mkdir -p /home/rancher/.docker/cli-plugins
+wget -q -O /home/rancher/.docker/cli-plugins/docker-compose $COMPOSE_URL
+chmod 0755 /home/rancher/.docker/cli-plugins/docker-compose
+ln -s /home/rancher/.docker/cli-plugins/docker-compose /var/lib/rancher/compose/docker-compose
